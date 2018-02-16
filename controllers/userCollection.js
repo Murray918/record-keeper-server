79eafde33297e735c3287f4ac8b63156878cc896
Andrew Murray <darbyguitars@gmail.com>
2018-02-16 17:06:54 -0600

    added todos for the next set of endpoints

--- controllers/userCollection.js
+++ controllers/userCollection.js
@@ -1,1 +1,21 @@
 const User = require('../models/user');
+
+exports.addReccord = function() {
+	//TODO add record to specific users collection
+};
+
+exports.removeRecrod = function() {
+	//TODO remove record from users collection
+};
+
+exports.viewRecords = function() {
+	//TODO display users record collection
+};
+
+exports.updateUser = function() {
+	//TODO update a users profile information
+};
+
+exports.deleteUser = function() {
+	//TODO remove a user from db
+};
