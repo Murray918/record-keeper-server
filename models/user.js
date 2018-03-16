c36ea600600980165688a3a9916f641f45b0e247
Andrew Murray <darbyguitars@gmail.com>
2018-03-16 11:50:32 -0500

    removed logger for now

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt-nodejs');
-const logger =
+// const logger =
 
 const Record = new Schema({
 	id: String,
